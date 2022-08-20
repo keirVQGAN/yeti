f251b0770746df9c8e03bcfd141003d60211babb
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-08-20 11:29:46 +0100

    Update and rename yetiSuper.sh to ini/yetiSuper.sh

---
+++ ini/yetiSuper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/xinntao/Real-ESRGAN.git               &> /dev/null
+cd /content/Real-ESRGAN
+pip install basicsr                                              &> /dev/null
+pip install -r requirements.txt                                  &> /dev/null
+python setup.py develop                                                      
+wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P experiments/pretrained_models
--- yetiSuper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-!git clone https://github.com/xinntao/Real-ESRGAN.git               &> /dev/null
-%cd /content/Real-ESRGAN
-!pip install basicsr                                              &> /dev/null
-!pip install -r requirements.txt                                  &> /dev/null
-!python setup.py develop                                                      
-!wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P experiments/pretrained_models
