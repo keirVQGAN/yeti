c7ecb97694d5560cafaa140f0b389fcc38dadab5
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-10-07 11:04:33 +0100

    Create txt2img_ini.sh

---
+++ txt2img_ini.sh
@@ -0,0 +1,11 @@
+cd /content                    
+pip install -r /content/yeti/requirements_txt2img.txt                        
+apt -qq install imagemagic       
+git clone --recurse-submodules -j8 https://github.com/pytti-tools/pytti-core 
+pip install ./pytti-core/vendor/AdaBins                                      
+pip install ./pytti-core/vendor/CLIP                                         
+pip install ./pytti-core/vendor/GMA                                          
+pip install ./pytti-core/vendor/taming-transformers                          
+pip install ./pytti-core                                                     
+python -m pytti.warmup                                                       
+touch /content/config/conf/empty.yaml
