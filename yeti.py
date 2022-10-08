5fb1163803ecb0adff2e173d20b3dc76d48daf9d
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-10-08 09:05:50 +0100

    Update yeti.py

--- yeti.py
+++ yeti.py
@@ -213,7 +213,7 @@ def thresh(imagePath , outPath) :
         imageio.imwrite ( f'{outPath}/{imageName}/{thresh}_mask.jpg' , img_binary )
         threshMasked.append ( f'{outPath}/{imageName}/{thresh}_mask.jpg' )
 
-def yml(quality):
+def yml(QUALITY):
     # --------------------------------------------------------------------------
     # QUALITY SETTINGS
     if QUALITY == 'test':
