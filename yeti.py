7ea5fd99e63178677aa45548185f0876aa77b8ef
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-10-09 20:15:00 +0100

    Update yeti.py

--- yeti.py
+++ yeti.py
@@ -249,7 +249,7 @@ def yml(QUALITY, csv_file, confPath, init_image):
     # QUALITY SETTINGS
     if QUALITY == 'test':
       _width = 300
-      _cut_outs = 40
+      _cut_outs = 128
       _cut_pow = 2.8
       _pixel_size = 2
       _direct_init_weight = 0.75
