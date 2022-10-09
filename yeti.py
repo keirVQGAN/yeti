0d72cc21ce29c4863d462929acf95b96b22c6bf1
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-10-09 10:17:33 +0100

    Update yeti.py

--- yeti.py
+++ yeti.py
@@ -251,7 +251,7 @@ def yml(QUALITY, csv_file, confPath, init_image):
       _width = 200
       _cut_outs = 32
       _cut_pow = 2.4
-      _pixel_size = 1
+      _pixel_size = 2
       _direct_init_weight = 0.75
       _gradient_accumulation_steps = 1
       _steps_per_scene = 500
