87afec6c2003cce1f9cf145bebf240f581ca22fd
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-10-13 20:20:06 +0100

    Add files via upload

---
+++ ini/yetiNiceface_ini.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/TencentARC/GFPGAN.git
+cd /content/GFPGAN
+pip install basicsr
+pip install facexlib
+pip install -r requirements.txt
+python setup.py develop
+pip install realesrgan
