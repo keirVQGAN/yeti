16f721653a49b399100904e5920401a37cfff630
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-10-24 19:35:34 +0100

    Add files via upload

---
+++ ini/diffuse-colabWebUi.sh
@@ -0,0 +1,9 @@
+user_token = "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"
+user_header = f"\"Authorization: Bearer {user_token}\""
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
+cd stable-diffusion-webui
+wget --header={user_header} https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt -O /content/stable-diffusion-webui/model.ckpt
+COMMANDLINE_ARGS="--exit" REQS_FILE="requirements.txt" python launch.py
+cd stable-diffusion-webui
+git pull
+COMMANDLINE_ARGS="--share --gradio-debug --gradio-auth me:qwerty" REQS_FILE="requirements.txt" python launch.py
