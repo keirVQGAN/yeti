815ecf52450f2af361081ac2e2bfeeceb8a7cf25
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-02-04 15:45:40 +0000

    Update yeti.py

--- yeti.py
+++ yeti.py
@@ -411,6 +411,8 @@ def wot(IMG):
       'model': "coco",
       'use_beam_search': False,
   }
+  model = replicate.models.get("rmokady/clip_prefix_caption")
+  version = model.versions.get("9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8")
 
   output = version.predict(**inputs)
   return output
