b266aecb5b2ea66cc433297ad981c5efda691092
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-04-20 23:52:54 +0100

    Update mani.py

--- mani.py
+++ mani.py
@@ -1,7 +1,15 @@
-import base64, json, random, subprocess
-from io import BytesIO
+import base64, json, random, subprocess, os, requests, openai
 import numpy as np
 from PIL import Image
+from io import BytesIO
+from datetime import datetime
+
+from rich.console import Console
+from rich.table import Table
+from rich.markdown import Markdown
+from IPython.display import display, Image
+from PIL import Image as PILImage
+
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Image as RLImage, Paragraph, Spacer
@@ -9,103 +17,6 @@ from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import inch
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-import os
-from datetime import datetime
-import openai
-import os
-import requests
-
-def image(MANI_TITLE, animal, api_key, prompt, init_image, width=512, height=512, samples=2, negative_prompt='Repeated Edge, Tiling', mask_image=None, prompt_strength=None, num_inference_steps=30, guidance_scale=3, enhance_prompt='yes', seed=None, webhook=None, track_id=None):
-    url = 'https://stablediffusionapi.com/api/v3/img2img'
-    headers = {'Content-Type': 'application/json'}
-    output_dir = f'/content/drive/MyDrive/mani/out/manifestos/{MANI_TITLE}'
-    data = {
-        "key": api_key,
-        "prompt": prompt,
-        "negative_prompt": negative_prompt,
-        "init_image": init_image,
-        "width": str(width),
-        "height": str(height),
-        "samples": str(samples),
-        "num_inference_steps": str(num_inference_steps),
-        "guidance_scale": guidance_scale,
-        "enhance_prompt": enhance_prompt,
-        "seed": seed,
-        "webhook": webhook,
-        "track_id": track_id
-    }
-    
-    if mask_image:
-        data["mask_image"] = mask_image
-    
-    if prompt_strength:
-        data["prompt_strength"] = prompt_strength
-    
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    
-    if response.status_code == 200:
-        response_data = response.json()
-        # save response data to a unique JSON file
-        filename = f'{animal}.json'
-        filepath = os.path.join(output_dir, filename)
-        with open(filepath, 'w') as f:
-            json.dump(response_data, f)
-        
-        # download the generated images
-        image_urls = response_data['output']
-        image_paths = []
-        for url in image_urls:
-            response = requests.get(url)
-            filename = f'{animal}.jpg'
-            filepath = os.path.join(output_dir, filename)
-            with open(filepath, 'wb') as f:
-                f.write(response.content)
-            image_paths.append(filepath)
-            
-        return image_paths
-    else:
-        raise Exception(f"API call failed with status code {response.status_code}: {response.text}")
-
-
-def save_json(title, text, keywords, example_text, date_time, links):
-    """Save the manifesto information to a JSON file and to an individual JSON file for each manifesto."""
-    # Prepare manifesto data
-    manifesto_data = {
-        "title": title,
-        "text": text,
-        "keywords": keywords,
-        "example_text": example_text,
-        "date_time": date_time,
-        "link": links
-    }
-    
-    # Create the JSON output file path for the single manifesto
-    single_json_output_path = f"/content/drive/MyDrive/mani/out/manifestos/{title}/{title}.json"
-
-    # Save the single manifesto data to a JSON file
-    with open(single_json_output_path, 'w') as f:
-        json.dump(manifesto_data, f, indent=4)
-
-    # Create the JSON output file path for the main JSON file
-    main_json_output_path = '/content/drive/MyDrive/mani/out/json/manifesto_data.json'
-
-    # Load existing data or create an empty list if the main JSON file does not exist
-    existing_data = []
-    if os.path.exists(main_json_output_path):
-        with open(main_json_output_path, 'r') as f:
-            existing_data = json.load(f)
-
-    # Check if the new manifesto data is a duplicate
-    is_duplicate = any(data["title"] == title for data in existing_data)
-
-    # Append the new manifesto data to the existing data if it's not a duplicate
-    if not is_duplicate:
-        existing_data.append(manifesto_data)
-
-        # Write the updated data back to the main JSON output file
-        with open(main_json_output_path, 'w') as f:
-            json.dump(existing_data, f, indent=4)
-
 
 def sample(manifestos_limit=5, sample_limit=500):
     """Select a random sample of manifestos and return their links and sampled text."""
@@ -185,36 +96,139 @@ def title(manifesto_text, model='gpt-3.5-turbo'):
     response = openai.ChatCompletion.create(model=model, messages=messages)
     return response.choices[0].message.content.strip().replace('"', '')
 
-def select_image(image_dir, color_space):
-    """
-    Select an image from a directory based on the specified color space.
+def image(MANI_TITLE, animal, api_key, prompt, init_image, width=512, height=512, samples=2, negative_prompt='Repeated Edge, Tiling', mask_image=None, prompt_strength=None, num_inference_steps=30, guidance_scale=3, enhance_prompt='yes', seed=None, webhook=None, track_id=None):
+    url = 'https://stablediffusionapi.com/api/v3/img2img'
+    headers = {'Content-Type': 'application/json'}
+    output_dir = f'/content/drive/MyDrive/mani/out/manifestos/{MANI_TITLE}'
+    data = {
+        "key": api_key,
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "init_image": init_image,
+        "width": str(width),
+        "height": str(height),
+        "samples": str(samples),
+        "num_inference_steps": str(num_inference_steps),
+        "guidance_scale": guidance_scale,
+        "enhance_prompt": enhance_prompt,
+        "seed": seed,
+        "webhook": webhook,
+        "track_id": track_id
+    }
+    
+    if mask_image:
+        data["mask_image"] = mask_image
+    
+    if prompt_strength:
+        data["prompt_strength"] = prompt_strength
+    
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    
+    if response.status_code == 200:
+        response_data = response.json()
+        # save response data to a unique JSON file
+        filename = f'{animal}.json'
+        filepath = os.path.join(output_dir, filename)
+        with open(filepath, 'w') as f:
+            json.dump(response_data, f)
+        
+        # download the generated images
+        image_urls = response_data['output']
+        image_paths = []
+        for url in image_urls:
+            response = requests.get(url)
+            filename = f'{animal}.jpg'
+            filepath = os.path.join(output_dir, filename)
+            with open(filepath, 'wb') as f:
+                f.write(response.content)
+            image_paths.append(filepath)
+            
+        return image_paths
+    else:
+        raise Exception(f"API call failed with status code {response.status_code}: {response.text}")
 
-    Args:
-    - image_dir (str): The directory containing the images.
-    - color_space (str): The desired color space of the selected image.
+def display_info(MANI_TITLE, MANI_TEXT, VALUE, LANDSCAPE, ELEMENT, ANIMAL, MANI_SAMPLE, MANI_SAMPLE_LINKS, image_paths):
+    console = Console()
+    table_width = int(console.width * 0.75)
 
-    Returns:
-    - The path of the selected image file.
-    """
-    # Get list of all image files in directory
-    images = os.listdir(image_dir)
+    main_table = Table(width=table_width, pad_edge=False, collapse_padding=True)
+    main_table.add_column(MANI_TITLE, style="white on black")
 
-    # Select an image based on the color space
-    if color_space == 'RGB':
-        image_path = os.path.join(image_dir, [f for f in images if f.endswith('.png')][0])
-    elif color_space == 'CMYK':
-        image_path = os.path.join(image_dir, [f for f in images if f.endswith('.jpg')][0])
+    # Main text
+    main_table.add_row(MANI_TEXT)
 
-    return image_path
+    # Table for keywords
+    keywords_table = Table(show_header=False, collapse_padding=True)
+    keywords_table.add_column("Keyword", style="bold white on black")
+    keywords_table.add_column("Value", style="white on black")
+    keywords_table.add_row("Value", VALUE)
+    keywords_table.add_row("Landscape", LANDSCAPE)
+    keywords_table.add_row("Element", ELEMENT)
+    keywords_table.add_row("Animal", ANIMAL)
+    main_table.add_row(keywords_table)
 
-def html(title, text):
+    # Sample text
+    main_table.add_row("Sample Text:")
+    main_table.add_row(MANI_SAMPLE)
+
+    # Sample links
+    main_table.add_row("Sample Links:")
+    links_str = "\n".join(f"[{link}]({link})" for link in MANI_SAMPLE_LINKS)
+    main_table.add_row(Markdown(links_str))
+
+    # Display first image in the list, resized to fit the table width
+    first_image_path = image_paths[0]
+    display(Image(filename=first_image_path, width='400', height='400', metadata={"padding": 10, "border": "white"}))
+
+    console.print(main_table)
+
+def save_json(title, text, keywords, example_text, date_time, links):
+    """Save the manifesto information to a JSON file and to an individual JSON file for each manifesto."""
+    # Prepare manifesto data
+    manifesto_data = {
+        "title": title,
+        "text": text,
+        "keywords": keywords,
+        "example_text": example_text,
+        "date_time": date_time,
+        "link": links
+    }
+    
+    # Create the JSON output file path for the single manifesto
+    single_json_output_path = f"/content/drive/MyDrive/mani/out/manifestos/{title}/{title}.json"
+
+    # Save the single manifesto data to a JSON file
+    with open(single_json_output_path, 'w') as f:
+        json.dump(manifesto_data, f, indent=4)
+
+    # Create the JSON output file path for the main JSON file
+    main_json_output_path = '/content/drive/MyDrive/mani/out/json/manifesto_data.json'
+
+    # Load existing data or create an empty list if the main JSON file does not exist
+    existing_data = []
+    if os.path.exists(main_json_output_path):
+        with open(main_json_output_path, 'r') as f:
+            existing_data = json.load(f)
+
+    # Check if the new manifesto data is a duplicate
+    is_duplicate = any(data["title"] == title for data in existing_data)
+
+    # Append the new manifesto data to the existing data if it's not a duplicate
+    if not is_duplicate:
+        existing_data.append(manifesto_data)
+
+        # Write the updated data back to the main JSON output file
+        with open(main_json_output_path, 'w') as f:
+            json.dump(existing_data, f, indent=4)
+
+def html(title, text, image_path):
     """
     Create an HTML file for a manifesto with the specified title, text, and image.
 
     Args:
     - title (str): The title of the manifesto.
     - text (str): The main text of the manifesto.
-    - image_dir (str): The directory containing the images.
+    - image_path (str): The path to the image file.
 
     Returns:
     - None
@@ -222,38 +236,27 @@ def html(title, text):
     # Load the HTML template
     with open('/content/drive/MyDrive/mani/in/html/manifesto_template.html') as f:
         html_template = f.read()
-  
-    # Remove the first sentence from the text
-    text = text.split('. ', 1)[1]
 
     # Wrap each paragraph in <p> tags
     paragraphs = [f'<p>{p.strip()}</p>' for p in text.split('\n\n')]
 
-    # Get a random image from the specified directory
-    image_dir = '/content/drive/MyDrive/mani/in/images/RGB'
-    images = [file for file in os.listdir(image_dir) if file.endswith('.png')]
-    if images:
-        image_path = os.path.join(image_dir, random.choice(images))
-        with Image.open(image_path) as img:
-            # Get the aspect ratio of the image
-            width, height = img.size
-            aspect_ratio = height / width
+    # Load the image
+    with PILImage.open(image_path) as img:
+        # Resize the image to fit inside the box and maintain the aspect ratio
+        width, height = img.size
+        aspect_ratio = height / width
+        new_width = 300
+        new_height = int(new_width * aspect_ratio)
+        img = img.resize((new_width, new_height))
 
-            # Resize the image to fit inside the box and maintain the aspect ratio
-            new_width = 300
-            new_height = int(new_width * aspect_ratio)
-            img = img.resize((new_width, new_height), resample=Image.LANCZOS)
+        # Convert the image to a base64-encoded string for embedding in the HTML
+        with open('temp_image.png', 'wb') as f:
+            img.save(f, format='png')
+        with open('temp_image.png', 'rb') as f:
+            image_data = base64.b64encode(f.read()).decode('utf-8')
 
-            # Convert the image to a base64-encoded string for embedding in the HTML
-            with open('temp_image.png', 'wb') as f:
-                img.save(f, format='png')
-            with open('temp_image.png', 'rb') as f:
-                image_data = base64.b64encode(f.read()).decode('utf-8')
-
-            # Remove the temporary image file
-            os.remove('temp_image.png')
-    else:
-        image_data = ''
+        # Remove the temporary image file
+        os.remove('temp_image.png')
 
     # Replace the placeholders with the specified title, text, and image
     html_content = html_template.replace("{{ title }}", title).replace("{{ text }}", ''.join(paragraphs)).replace("{{ image }}", f'data:image/png;base64,{image_data}')
@@ -261,23 +264,9 @@ def html(title, text):
     # Save the HTML content to a file with a unique name based on the title
     filename = f'{title.lower().replace(" ", "_")}.html'
     with open(f'/content/drive/MyDrive/mani/out/manifestos/{title}/{filename}', 'w') as f:
-        f.write(html_content)   
+        f.write(html_content)
 
-import random
-from reportlab.lib.pagesizes import A4
-from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Image as RLImage, Paragraph, Spacer
-from reportlab.lib.styles import ParagraphStyle
-from reportlab.lib.units import inch
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-
-def create_pdf(title, text):
-    # Randomly select an image from the specified folder
-    image_folder = "/content/drive/MyDrive/mani/in/images/CMYK"
-    image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(".jpg")]
-    image_path = os.path.join(image_folder, random.choice(image_files))
-
+def create_pdf(title, text, image_path):
     # Prepare output file path
     output_folder = f"/content/drive/MyDrive/mani/out/manifestos/{title}"
     os.makedirs(output_folder, exist_ok=True)
@@ -324,5 +313,3 @@ def create_pdf(title, text):
     # Build document
     flowables = [image, Spacer(1, 12), title_paragraph, Spacer(1, 12), subtitle_paragraph, Spacer(1, 12), body_paragraph]
     doc.build(flowables)
-            
-
